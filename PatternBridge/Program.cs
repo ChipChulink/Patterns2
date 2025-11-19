b8ca3e3a6a74da40479ca1e0077b3a243c16cfeb
Ivan <108826351+ChipChulink@users.noreply.github.com>
2025-11-19 10:20:47 +0300

    Bridge

---
+++ PatternBridge/Program.cs
@@ -0,0 +1,74 @@
+﻿public interface ILogger
+{
+    public void Log(string message);
+}
+
+public class FileLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"File: {message}");
+    }
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"Console: {message}");
+    }
+}
+
+public abstract class StoreLogger
+{
+    protected ILogger _logger;
+
+    public StoreLogger(ILogger logger)
+    {
+        _logger = logger;
+    }
+
+    public abstract void LogAction(string action);
+}
+
+public class OrderLogger : StoreLogger
+{
+    public OrderLogger(ILogger logger) : base(logger) { }
+
+    public override void LogAction(string action)
+    {
+        _logger.Log($"[ORDER] {action}");
+    }
+}
+
+public class UserLogger : StoreLogger
+{
+    public UserLogger(ILogger logger) : base(logger) { }
+
+    public override void LogAction(string action)
+    {
+        _logger.Log($"[USER] {action}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        var fileLogger = new FileLogger();
+        var consoleLogger = new ConsoleLogger();
+
+
+        var orderFileLogger = new OrderLogger(fileLogger);
+        var userConsoleLogger = new UserLogger(consoleLogger);
+
+
+        orderFileLogger.LogAction("Order #1 created");
+        userConsoleLogger.LogAction("User Ivan logged in");
+
+
+        var orderConsoleLogger = new OrderLogger(consoleLogger);
+        orderConsoleLogger.LogAction("Order #1 paid");
+    }
+}
