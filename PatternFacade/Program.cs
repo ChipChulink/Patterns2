56c5e784d726e074e96a7c901b4b5790b7e89c66
Ivan <108826351+ChipChulink@users.noreply.github.com>
2025-11-19 10:26:26 +0300

    Facade

---
+++ PatternFacade/Program.cs
@@ -0,0 +1,36 @@
+﻿public class UserDatabase
+{
+    public void SaveUser(string user)
+    {
+        Console.WriteLine($"User saved: {user}");
+    }
+}
+
+public class OrderDatabase
+{
+    public void SaveOrder(string order)
+    {
+        Console.WriteLine($"Order saved: {order}");
+    }
+}
+
+public class DatabaseGateway
+{
+    private UserDatabase _userDb = new UserDatabase();
+    private OrderDatabase _orderDb = new OrderDatabase();
+
+    public void SaveData(string user, string order)
+    {
+        _userDb.SaveUser(user);
+        _orderDb.SaveOrder(order);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var gateway = new DatabaseGateway();
+        gateway.SaveData("Ivan", "Order1");
+    }
+}
